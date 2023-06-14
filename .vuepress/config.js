a9ee3421950f04e59bbf211ffa1a89e37252fd13
yinchen <yinchenwang1998@gmail.com>
2023-06-14 12:15:49 +0200

    first version

---
+++ .vuepress/config.js
@@ -0,0 +1,99 @@
+module.exports = {
+    "title": "yincBlog",
+    "description": "blog of yinc",
+    "dest": "public",
+    "head": [
+        [
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width,initial-scale=1,user-scalable=no"
+            }
+        ]
+    ],
+    "theme": "reco",
+    "themeConfig": {
+        "nav": [
+            {
+                "text": "Home",
+                "link": "/",
+                "icon": "reco-home"
+            },
+            {
+                "text": "TimeLine",
+                "link": "/timeline/",
+                "icon": "reco-date"
+            },
+            {
+                "text": "Docs",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "vuepress-reco",
+                        "link": "/docs/theme-reco/"
+                    }
+                ]
+            },
+            {
+                "text": "Contact",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "GitHub",
+                        "link": "https://github.com/recoluan",
+                        "icon": "reco-github"
+                    }
+                ]
+            }
+        ],
+        "sidebar": {
+            "/docs/theme-reco/": [
+                "",
+                "theme",
+                "plugin",
+                "api"
+            ]
+        },
+        "type": "blog",
+        "blogConfig": {
+            "category": {
+                "location": 2,
+                "text": "Category"
+            },
+            "tag": {
+                "location": 3,
+                "text": "Tag"
+            }
+        },
+        "friendLink": [
+            {
+                "title": "稚辉君",
+                "desc": "快来膜拜大佬！！.",
+                "link": "https://github.com/peng-zhihui"
+            },
+            {
+                "title": "代码随想录",
+                "desc": "代码随想录网站",
+                "link": "https://programmercarl.com/"
+            }
+        ],
+        "logo": "/logo.png",
+        "search": true,
+        "searchMaxSuggestions": 10,
+        "lastUpdated": "Last Updated",
+        "author": "Yinc",
+        "authorAvatar": "/avatar.png",
+        "record": "xxxx",
+        "startYear": "2017"
+    },
+    "markdown": {
+        "lineNumbers": true
+    }
+}
